02d5bb8c2bc14a2029fe36976cafe3699d6dbd62
Daniel Baldwin <dbald@stilito.com>
2010-11-23 23:39:36 -0700

    fixed migrations

--- migrations/1_add_user_table.php
+++ migrations/1_add_user_table.php
@@ -3,25 +3,22 @@ class AddUserTable extends Doctrine_Migration_Base {
     public function up() {
 		$columns = array(
 			'id' => array(
-				'type' => 'integer',
-				'length' => 11,
-				'primary' => true,
-				'autoincrement' => true
+				'type' => 'string',
+				'length' => '36',
+				'primary' => true
 			),
 			'username' => array(
 				'type' => 'string',
-				'length' => 255,
-				'notnull' => true,
-				'primary' => true
+				'length' => '255',
+				'notnull' => true
 			),
 			'password' => array(
 				'type' => 'string',
-				'length' => 255,
+				'length' => '255',
 				'notnull' => true
 			)
 		);
 		
-		$this->dropTable('users');
         $this->createTable('users', $columns);
     }
 
