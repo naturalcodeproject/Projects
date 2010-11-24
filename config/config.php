653aabb2f9f5d3ff957593d27427bc9f4dfe968c
Daniel Baldwin <dbald@stilito.com>
2010-11-24 11:05:08 -0700

    Abstracted the database setup

--- config/config.php
+++ config/config.php
@@ -26,21 +26,6 @@
 		'Cache.expires'	=> 3600, // 1 hour
 	));
 	
-	Autoloader::registerPrefix('Doctrine_', Reg::get("Path.physical").'/vendors/Doctrine-1.2.3');
-	set_include_path(Reg::get('Path.physical').'/vendors/Doctrine-1.2.3'.PATH_SEPARATOR.get_include_path());
-	
-	/* Database Setup */ //http://www.doctrine-project.org/projects/orm/1.2/docs/manual/connections/en
-	$manager = Doctrine_Manager::getInstance();
-	$manager->setAttribute(Doctrine_Core::ATTR_MODEL_LOADING, Doctrine_Core::MODEL_LOADING_CONSERVATIVE);
-	
-	$conn = Doctrine_Manager::connection('mysql://root:root@localhost/projects', 'Test Connection');
-	//Autoloader::registerDirectory('models', Reg::get("Path.physical").'/models');
-	
-	// Migrations
-	try {	
-		$migration = new Doctrine_Migration(Reg::get("Path.physical").'/migrations', $conn);
-		$migration->migrate();
-	} catch(Exception $e) {
-		// do nothing...
-	}
+	/* Database Setup */
+	include('database.php');
 ?>
