a2100c0337bcd61f7c7808576f03915fa6f9c62f
Daniel Baldwin <dbald@stilito.com>
2010-12-14 18:27:08 -0700

    Added the login view

--- views/main/login.php
+++ views/main/login.php
@@ -1,7 +1,7 @@
 <h2>Login</h2>
 
 <div class="login-box">
-	<form method="post" action="">
+	<form method="post" action="[site]">
 		<div class="form-field">
 			<label for="username">Username</label>
 			<input type="text" id="username" name="username" />
@@ -9,6 +9,9 @@
 		<div class="form-field">
 			<label for="password">Password</label>
 			<input type="text" id="password" name="password" />
+		</div>
+		<div class="form-field">
+			<input type="submit" id="submit" value="Login" />
 		</div>
 	</form>
 </div>
